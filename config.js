888a0267d2387846d17551cd8101bb40be00f075
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2020-06-03 19:24:26 -0500

    Se ha actualizado la uri para conectar con la base de datos de mlab.com

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3700;
 const config = {
     port,
     url: `http://192.168.0.4:${port}/`,
-    dataBase: 'mongodb://localhost:27017/portafolio'
+    dataBase: "mongodb+srv://default-user:default-user@portfoliodb-luwjq.gcp.mongodb.net/portfoliodb?retryWrites=true&w=majority"  //'mongodb://localhost:27017/portafolio'
 } 
 
 module.exports = config;
